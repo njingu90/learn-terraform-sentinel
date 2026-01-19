5b7c6748f2e883c7d21b7b33977808ba09fca5a5
njingu90 <85954427+njingu90@users.noreply.github.com>
2026-01-19 17:37:02 +0000

    Add Sentinel policy validation

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-
   tags = {
     Name = var.instance_name
   }
